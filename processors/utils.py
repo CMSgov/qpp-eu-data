ca388479796edd92cd40e716d1463f0b451ea422
Sivakumar Srinivasulu <siva.srinivasulu@semanticbits.com>
2023-01-10 09:33:55 -0600

    updated tests to remove the functions that are not being used

--- processors/utils.py
+++ processors/utils.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from pydash import strings
 from dateutil.relativedelta import relativedelta
 from datetime import datetime, timezone
-import dateutil.parser as parser
 
 class Config:
 
@@ -44,15 +43,6 @@ class Utils:
         for s in to_delete:
             s1 = strings.replace(s1, s, "", ignore_case=True)
         return s1
-    
-    @classmethod
-    def to_int(cls, s):
-        if not s or not cls.is_number(s): return s
-        return int(s)
-
-    @classmethod
-    def is_number(cls, s):
-        return strings.reg_exp_js_match(s, '/^-?\d+(?:\.\d+)?$/')
 
     @classmethod
     def subtract_months(cls, from_day, months):
@@ -61,49 +51,6 @@ class Utils:
     @classmethod
     def add_to_date(cls, from_day, years = 0, weeks =0, months = 0, days =0, hours=0, minutes = 0, seconds = 0):
         return from_day + relativedelta(years = years, months = months, weeks = weeks, days = days, hours = hours, minutes = minutes, seconds= seconds)
-    
-    @classmethod
-    def parse_date(cls, text):
-        if not text: return None
-        if isinstance(text, datetime):
-            return text
-
-        if cls.is_number(text):
-            return datetime.fromtimestamp(Utils.to_int(text)/1000.0)
-
-        if not text.__contains__("T"):
-            return parser.parse(text)
-
-        # apply UTC correction - as isofomat is not not adjusting time when tzinfo is UTC properly
-        t = strings.split(text, "T")
-        date_part = t[0]
-        time_offset_part = t[1] if cls.size(t) > 1 else "00:00:00"
-        time_part = time_offset_part
-        offset_part = "00:00"
-        operator = 1
-        if time_offset_part.__contains__("+"):
-            operator = 1
-            parts = strings.split(time_offset_part, "+")
-            time_part = parts[0]
-            offset_part = parts[1] if cls.size(parts) > 1 else "00:00"
-        elif time_offset_part.__contains__("-"):
-            operator = -1
-            parts = strings.split(time_offset_part, "-")
-            time_part = parts[0]
-            offset_part = parts[1] if cls.size(parts) > 1 else "00:00"
-
-        new_date_text = f'{date_part}T{time_part}Z'
-        hours = 0
-        minutes = 0
-        if offset_part != "00:00":
-            hours_minutes = offset_part.split(":")
-            hours = operator * int(hours_minutes[0])
-            minutes = operator * int(hours_minutes[1]) if cls.size(hours_minutes) > 1 else 0
-
-        d = parser.parse(new_date_text)
-        d = cls.add_to_date(d, hours = hours, minutes = minutes)
-
-        return d
 
     @classmethod
     def format_date(cls, the_date):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-import unittest, requests
+import requests
 from unittest import TestCase
 from unittest import mock, TestCase
 from unittest.mock import Mock
@@ -8,23 +8,9 @@ from processors.utils import Utils, APIUtils
 class TestUtils(TestCase):
 
     def test_format_date(self):
-        d = Utils.parse_date("2021-06-22 06:42:15.253671")
-        self.assertIsNotNone(Utils.format_date(Utils.now()))
+        d = Utils.now()
+        self.assertIsNotNone(Utils.format_date(d))
         self.assertIsNone(None)
-
-    def test_parse_date(self):
-        d3 = "2021-06-24T19:00:30-05:00"
-        d4 = "2021-06-24T19:00:30+00:00"
-        diff = Utils.elapsed_time(d4, d3)
-        self.assertEqual(18000, diff)
-        self.assertEqual("5 hours", Utils.human_time_duration(diff))
-
-    def test_parse_date(self):
-        d = Utils.parse_date("2021-06-22 06:42:15.253671")
-        self.assertEqual("2021-06-22T06:42:15.253671", Utils.format_date(d))
-
-        # d = Utils.parse_date("1664301600000")
-        # self.assertEqual("2022-09-27T18:00:00", Utils.format_date(d))
 
     def test_http_get(self):
         with mock.patch.object(requests, "get", return_value = Mock(status_code=200, json=lambda : {"data": {"id": "test"}})) as g:
